221633b64eedf5a2f6369e257321cd65ff5d6a81
fahranmf <fahranmaulana182@gmail.com>
2025-11-12 19:13:45 +0700

    news app

---
+++ app/Filament/Resources/KomentarResource/Pages/ListKomentars.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Filament\Resources\KomentarResource\Pages;
+
+use App\Filament\Resources\KomentarResource;
+use Filament\Resources\Pages\ListRecords;
+
+class ListKomentars extends ListRecords
+{
+    protected static string $resource = KomentarResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [];
+    }
+}
